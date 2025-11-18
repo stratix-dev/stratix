2295d8af9f202011d8a9f33e836756f37e8fb012
Andrés Carvajal <causticrez@gmail.com>
2025-11-18 12:38:44 -0300

    fix version and commits

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name('stratix')
   .description('Stratix CLI - Build DDD applications with ease')
-  .version('0.1.5');
+  .version('0.1.7');
 
 program.addCommand(createNewCommand());
 program.addCommand(createGenerateCommand());
