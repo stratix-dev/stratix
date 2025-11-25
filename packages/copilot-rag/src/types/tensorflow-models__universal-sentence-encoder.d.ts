11c000584edeae89c0b8be5821f89a431e326ad1
Andrés Carvajal <causticrez@gmail.com>
2025-11-25 20:15:53 -0300

    fix: Extract Stratix Copilot to standalone repo

--- packages/copilot-rag/src/types/tensorflow-models__universal-sentence-encoder.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@tensorflow-models/universal-sentence-encoder' {
-  import * as tf from '@tensorflow/tfjs';
-
-  export interface UniversalSentenceEncoder {
-    embed(inputs: string[] | string): Promise<tf.Tensor2D>;
-  }
-
-  export function load(): Promise<UniversalSentenceEncoder>;
-}
